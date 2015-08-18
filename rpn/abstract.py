e918e61477f413676b6a8a66f7b2c132565a871e
Gemma Hentsch <gemma@halfapenguin.com>
2015-08-18 12:27:13 +0100

    fixes the superclassing

--- rpn/abstract.py
+++ rpn/abstract.py
@@ -5,22 +5,21 @@ from operator import add, sub, or_, mul, div
 
 import re
 
-class AbstractEvaluator:
-    operators = {}
-    is_number = re.compile(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?")
-    def __init__(self, *args, **kwargs):
-        super(AbstractEvaluator, self).__init__(*args, **kwargs)
-        self.stack = deque()
-    
-    def evaluate(self, expression):
-        tokens = self.tokenize(expression)
-        for token in tokens:
-            if type(token) == builtin_function_or_method:
-                token(self.stack)
-            else:
-                self.stack.append(token)
-        return stack.pop()          
-
+class AbstractEvaluator(object):
+	operators = {}
+	is_number = re.compile(r"[-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?")
+	def __init__(self, *args, **kwargs):
+		super(AbstractEvaluator, self).__init__(*args, **kwargs)
+		self.stack = deque()
+	
+	def evaluate(self, expression):
+		tokens = self.tokenize(expression)
+		for token in tokens:
+			if type(token) == builtin_function_or_method:
+				token(self.stack)
+			else:
+				self.stack.append(token)
+		return stack.pop()
 
     def tokenize(self, expression):
         processed_tokens = []
