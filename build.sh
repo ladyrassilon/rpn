713a9992ee92516dd716468a16c08ca2be9aebec
Gemma Hentsch <gemma@halfapenguin.com>
2015-08-19 14:10:35 +0100

    installs pylint

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 python setup.py nosetests --with-xunit --with-coverage --cover-package rpn --cover-erase --cover-xml --cover-branch --cover-html
+pip install pylint
 pylint -f parseable -d I0011,R0801 rpn | tee pylint.out
