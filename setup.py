b38ab8e915955cad858aa4c93220e3b1971314d6
Gemma Hentsch <gemma@halfapenguin.com>
2015-08-19 11:35:51 +0100

    whitespace fixes

--- setup.py
+++ setup.py
@@ -4,34 +4,33 @@ with open("README.rst") as readme_file:
     long_description = readme_file.read()
 
 
-setup(name="RPyN",
+setup(
+    name="RPyN",
     version="0.1",
-    packages=["rpn",],
+    packages=["rpn"],
     license="GNU GPL v3.0",
     description="Evaluates a given Reverse Polish Notation",
     author="Gemma Hentsch",
     author_email="contact@halfapenguin.com",
-    install_requires=[
-        
-    ],
-    tests_require=[ 
-        "coverage",
-        "nose",
-        "pytest"
+    install_requires=[],
+    tests_require=[
+      "coverage",
+      "nose",
+      "pytest"
     ],
     long_description=long_description,
     test_suite="nose.collector",
     url="https://github.com/zooeysoftware/rpn",
-    keywords = ["rpn", "math", "formula"],
+    keywords=["rpn", "math", "formula"],
     download_url="https://github.com/zooeysoftware/rpn/archive/",
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Programming Language :: Python",
-        "Intended Audience :: Developers",
+      "Development Status :: 4 - Beta",
+      "Programming Language :: Python :: 3.3",
+      "Programming Language :: Python :: 3.4",
+      "Programming Language :: Python :: 2.7",
+      "Programming Language :: Python :: Implementation :: CPython",
+      "Programming Language :: Python :: Implementation :: PyPy",
+      "Programming Language :: Python",
+      "Intended Audience :: Developers",
     ]
 )
