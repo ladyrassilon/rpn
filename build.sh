bd1d531b7b2f65782a7436403833f24d87a0bdc3
Gemma Hentsch <gemma@halfapenguin.com>
2016-03-30 15:02:54 +0100

    sympy import problem fixed

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+git clone git://github.com/sympy/sympy.git
+python sympy/setup.py develop
 pip install pylint pep8 nose
 pep8 rpn tests > pep8_report.txt
 python setup.py nosetests --with-xunit --with-coverage --cover-package rpn --cover-erase --cover-xml --cover-branch --cover-html
 pylint -f parseable -d I0011,R0801 rpn | tee pylint.out
+rm -rf sympy
