2f5d2d16a66191818bcba29cb08982849dcbdc78
Gemma Hentsch <gemma@halfapenguin.com>
2016-03-30 15:02:24 +0100

    restructuring build scripts

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -4,10 +4,7 @@ dt="rpn-"$(date '+%d-%m-%Y-%H-%M-%S');
 mkvirtualenv "$dt"
 workon "$dt"
 
-pip install --no-cache pylint pep8 nose
-pep8 rpn tests > pep8_report.txt
-python setup.py nosetests --with-xunit --with-coverage --cover-package rpn --cover-erase --cover-xml --cover-branch --cover-html
-pylint -f parseable -d I0011,R0801 rpn | tee pylint.out
+./build.sh
 
 deactivate
 rmvirtualenv "$dt"
